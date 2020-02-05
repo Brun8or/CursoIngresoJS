1dbda2bbebd6753390951b1d279779c00ba4db99
Brun8or <60511065+Brun8or@users.noreply.github.com>
2020-02-05 17:32:07 -0300

    Ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -1,47 +1,77 @@
 /*Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
-mostar el resulto por medio de "ALERT"
+mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	var primerNumero;
 	/*las var no pueden empezar con numeros
 	no pueden tener espacio. Ej: 1Alumno=Mal, un Alumno=Mal,
 	unAlumno=Bien
 	*/
-	var segundoNumero;
 
+
+	var primerNumero;
+	var segundoNumero;
 	var resultadoDeLaSuma;
-	
+
 	primerNumero=document.getElementById('numeroUno').value;
 	primerNumero=parseInt(primerNumero);
 	segundoNumero=document.getElementById('numeroDos').value;
 	segundoNumero=parseInt(segundoNumero);
+
 	resultadoDeLaSuma=primerNumero+segundoNumero;
 
 
 	console.log(resultadoDeLaSuma);
-
-
-	/*3 formas de mostrar mensajes:1-Alert(Obsoleta, molesta)
-	                               2-ID(No sirve para desarrollo pero si para el final, intesteable)
-	                               3-Console.Log("")(Hay otros puntos investigables)
-*/
-
 }
 
 function restar()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultadoDeLaResta;
+
+
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+
+	resultadoDeLaResta=primerNumero-segundoNumero;
+
+
+	console.log(resultadoDeLaResta);
+
 }
 
 function multiplicar()
 { 
-	
+	var primerNumero;
+	var segundoNumero;
+	var producto;
+
+
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+
+	producto=primerNumero*segundoNumero;
+
+	console.log(producto);
+
+
+
 }
 
 function dividir()
 {
+	var primerNumero;
+	var segundoNumero;
+	var cociente;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	segundoNumero=document.getElementById('numeroDos').value;
+
+	cociente=primerNumero/segundoNumero;
+
+	console.log(cociente);
 	
 }
 

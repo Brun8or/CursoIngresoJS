52c7e68a4d1f9cefe25737913ff0e1bf841810db
Brun8or <60511065+Brun8or@users.noreply.github.com>
2020-02-05 17:04:03 -0300

    Ejercicios del 1 al 5 completos

---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,20 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
+*/
+function mostrar()
+{
+	var nombre;
+
+	//nombre=document.getElementById('elNombre').value;
+	nombre=prompt("Por favor introduzca su nombre");
+	document.getElementById('elNombre').value;
+
+
+
+	alert("Su nombre es "+nombre+" cierto?");
+
+
+
+}
+
